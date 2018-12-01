d2bccc63a84ced714d607ded3d78a6e92b6e7a4f
IINamelessII <justnitro26@gmail.com>
2018-12-01 16:02:17 +0200

    Add filter with preserving to main and search pages

---
+++ commit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "hello world"
+echo "git add ."
+git add .
+echo "git commit -m \"Add filter with preserving to main and search pages\""
+git commit -m "Add filter with preserving to main and search pages"
+echo "git push"
+git push
