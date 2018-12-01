8911582bb11103dd417ae5d8dea6babd23e766fd
IINamelessII <justnitro26@gmail.com>
2018-12-01 16:03:23 +0200

    Change commit script

--- commit.sh
+++ commit.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo "hello world"
-echo "git add ."
 git add .
-echo "git commit -m \"Add filter with preserving to main and search pages\""
-git commit -m "Add filter with preserving to main and search pages"
-echo "git push"
+git commit -m "Change commit script"
 git push
