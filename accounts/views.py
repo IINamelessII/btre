1c0119dbb7723d8522c92cfffe061d35b617ea07
IINamelessII <justnitro26@gmail.com>
2018-12-02 09:15:22 +0200

    Add registration and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,58 @@
 from django.shortcuts import redirect, render
-from django.contrib import messages
+from django.contrib import auth, messages
+from django.contrib.auth.models import User
 
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+        # Check if passwods match
+        if password != password2:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+        else:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                # Check email
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    # Create user
+                    user = User.objects.create_user(username=username, 
+                    password=password, email=email, first_name=first_name, 
+                    last_name=last_name)
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
     else:
         return render(request, 'accounts/register.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
